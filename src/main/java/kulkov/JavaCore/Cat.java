4dd4cbf4e0a9d447599d71397d7a28d8c4cb3ee9
Carou5el <garrydeepcool@outlook.com>
2021-02-11 12:37:23 +0300

    All tasks.

---
+++ src/main/java/kulkov/JavaCore/Cat.java
@@ -0,0 +1,63 @@
+package kulkov.JavaCore;
+
+import java.util.Random;
+
+public class Cat {
+
+
+    private final int MIN_HIGH_LIMIT = 70;                          // ..все могут прыгнуть на 70 см, как минимум.
+    private final int MAX_HIGH_LIMIT = 150;                         // предел высоты прыжка
+    private final int MIN_DIST_LIMIT = 1000;                        // ..все могут пробежать хотя бы 10 м
+    private final int MAX_DIST_LIMIT = 30000;                       // условный предел по бегу - 300 м
+
+    private int highLimit;
+    private int distanceLimit;
+    private boolean flagToGo = true;
+
+    public int getHighLimit() {
+        return highLimit;
+    }
+
+    public int getDistanceLimit() {
+        return distanceLimit;
+    }
+
+    public void setHighLimit(int highLimit) {
+        this.highLimit = highLimit;
+    }
+
+    public void setDistanceLimit(int distanceLimit) {
+        this.distanceLimit = distanceLimit;
+    }
+
+    // Constructor.
+    public Cat() {
+        Random generator = new Random();
+        this.highLimit = generator.nextInt((MAX_HIGH_LIMIT - MIN_HIGH_LIMIT)) + MIN_HIGH_LIMIT;
+        this.distanceLimit  = generator.nextInt((MAX_DIST_LIMIT - MIN_DIST_LIMIT)) + MIN_DIST_LIMIT;
+    }
+
+    // Methods.
+    // Method for Walls.
+    public boolean jump(Wall wall)   {
+        boolean result;
+        if(this.flagToGo && (this.highLimit >= wall.getHighWall()))  {
+            result = true;
+        } else {
+            this.flagToGo = false;
+            result = false;
+        }
+        return result;
+    }
+    // Method for RunTracks.
+    public boolean run(RunTrack runTrack)   {
+        boolean result;
+        if(this.flagToGo && (this.distanceLimit >= runTrack.getRunLength()))  {
+            result = true;
+        } else {
+            this.flagToGo = false;
+            result = false;
+        }
+        return result;
+    }
+}
--- src/main/java/kulkov/JavaCore/Main.java
+++ src/main/java/kulkov/JavaCore/Main.java
@@ -19,20 +19,30 @@ import java.util.Random;
 
  */
 public class Main {
-    public static int PERS_QNTY = 5;
-    public static int BARRIERS_QNTY = 5;
+    public static int PERS_QNTY = 10;
+    public static int BARRIERS_QNTY = 10;
 
     public static void main(String[] args) {
         Random generator = new Random();
         // Матрица результатов.
-        boolean[][] results = new boolean[BARRIERS_QNTY][PERS_QNTY];
+        boolean[][] result_humans = new boolean[BARRIERS_QNTY][PERS_QNTY];
+        boolean[][] result_robots = new boolean[BARRIERS_QNTY][PERS_QNTY];
+        boolean[][] result_cats = new boolean[BARRIERS_QNTY][PERS_QNTY];
         // Массив препятствий.
         Barrier[] barriers = new Barrier[BARRIERS_QNTY];
         // Массив персонажей.
         Human[] humans = new Human[PERS_QNTY];
+        Robot[] robots = new Robot[PERS_QNTY];
+        Cat[] cats = new Cat[PERS_QNTY];
         // Инициализация массива персонажей.
         for(int i = 0; i < humans.length; i++)  {
             humans[i] = new Human();
+        }
+        for(int i = 0; i < robots.length; i++)  {
+            robots[i] = new Robot();
+        }
+        for(int i = 0; i < cats.length; i++)  {
+            cats[i] = new Cat();
         }
 
         // Массив разнообразных препятствий.
@@ -53,12 +63,67 @@ public class Main {
             // Вызов преодолевания препятствий персонажем для каждого препятствия.
             for(int j = 0; j < PERS_QNTY; j++) {
                 if(barriers[i] instanceof Wall) {
-                    results[i][j] = humans[j].jump((Wall) barriers[i]);
+                    result_humans[i][j] = humans[j].jump((Wall) barriers[i]);
                 } else if(barriers[i] instanceof RunTrack)  {
-                    results[i][j] = humans[j].run((RunTrack) barriers[i]);
+                    result_humans[i][j] = humans[j].run((RunTrack) barriers[i]);
                 }
-
+            }
+            for(int j = 0; j < PERS_QNTY; j++) {
+                if(barriers[i] instanceof Wall) {
+                    result_robots[i][j] = robots[j].jump((Wall) barriers[i]);
+                } else if(barriers[i] instanceof RunTrack)  {
+                    result_robots[i][j] = robots[j].run((RunTrack) barriers[i]);
+                }
+            }
+            for(int j = 0; j < PERS_QNTY; j++) {
+                if(barriers[i] instanceof Wall) {
+                    result_cats[i][j] = cats[j].jump((Wall) barriers[i]);
+                } else if(barriers[i] instanceof RunTrack)  {
+                    result_cats[i][j] = cats[j].run((RunTrack) barriers[i]);
+                }
             }
         }
+
+        // Вывод результатов в консоль.
+        System.out.printf("pers / #\t");
+        for(int i = 0; i < BARRIERS_QNTY; i++) {
+            System.out.printf("%d\t", i);
+        }
+        System.out.printf("\n");
+        // Для Human.
+        for(int i = 0; i < PERS_QNTY; i++)  {
+            System.out.printf("hum[%d]\t", i);
+            for(int j = 0; j < BARRIERS_QNTY; j++)  {
+                System.out.printf("%b\t", result_humans[j][i]);
+            }
+            System.out.printf("\n");
+        }
+        System.out.printf("\n");
+        // Для Robot.
+        for(int i = 0; i < PERS_QNTY; i++)  {
+            System.out.printf("rob[%d]\t", i);
+            for(int j = 0; j < BARRIERS_QNTY; j++)  {
+                System.out.printf("%b\t", result_robots[j][i]);
+            }
+            System.out.printf("\n");
+        }
+        System.out.printf("\n");
+        // Для Cat.
+        for(int i = 0; i < PERS_QNTY; i++)  {
+            System.out.printf("cat[%d]\t", i);
+            for(int j = 0; j < BARRIERS_QNTY; j++)  {
+                System.out.printf("%b\t", result_cats[j][i]);
+            }
+            System.out.printf("\n");
+        }
+
     }
+
+//    public static <T> void initPersArray(T[] array)  {
+//        for(int i = 0; i < array.length; i++) {
+//            array[i] = new array[i].getClass()
+//        }
+//
+//
+//    }
 }
